71a457a4604519c63bae1e0e72dbc49f93b8f313
lou1sse <louisseevale@icloud.com>
2024-07-01 14:56:47 +0800

    feat(main.tsx): set up react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable prettier/prettier */
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { RouterProvider } from "react-router-dom"
 import "./index.css"
-
 import router from "./router"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
