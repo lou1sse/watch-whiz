0bfb2737c11fc168ca1d7351a11d7317db739d3b
Louisse Evale <louissemevale@gmail.com>
2024-07-04 13:10:12 +0800

    feat(TopRatedComponent): use ClickableDivComponent and onClickMovieItem

--- src/pages/components/TopRatedComponent.tsx
+++ src/pages/components/TopRatedComponent.tsx
@@ -1,9 +1,9 @@
+import { ClickableDivComponent } from "@GlobalComponents"
+import { MovieItem } from "@Store"
+import { formatDate, useCommonMethods } from "@Utilities"
 import { map } from "lodash-es"
 import { Autoplay } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
-
-import { MovieItem } from "@Modules"
-import { formatDate } from "@Utilities"
 import styles from "./scss/styles.module.scss"
 
 type Props = {
@@ -12,35 +12,27 @@ type Props = {
 
 function TopRatedComponent(props: Props) {
   const { data } = props
+  const { onClickMovieItem } = useCommonMethods()
 
   return (
     <div className={styles.topRatedComponent}>
       <p className={styles.title}>Top Rated</p>
       <Swiper
         breakpoints={{
-          768: {
-            slidesPerView: 5,
-            spaceBetween: 16
-          },
-          700: {
-            slidesPerView: 3,
-            spaceBetween: 16
-          },
-          0: {
-            slidesPerView: 2,
-            spaceBetween: 8
-          }
+          768: { slidesPerView: 5, spaceBetween: 16 },
+          700: { slidesPerView: 3, spaceBetween: 16 },
+          0: { slidesPerView: 2, spaceBetween: 8 }
         }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false
-        }}
+        autoplay={{ delay: 3000 }}
         modules={[Autoplay]}
         className={styles.swiper}
       >
         {map(data, (item) => (
           <SwiperSlide key={item.id}>
-            <div className={styles.swiper__item}>
+            <ClickableDivComponent
+              className={styles.swiper__item}
+              onClick={() => onClickMovieItem(item.id)}
+            >
               <img src={item.poster_url} alt={`${item.title} Poster`} />
               <div className={styles.textWrapper}>
                 <p>{item.title}</p>
@@ -48,7 +40,7 @@ function TopRatedComponent(props: Props) {
                   {formatDate(item.release_date, "YYYY")}
                 </p>
               </div>
-            </div>
+            </ClickableDivComponent>
           </SwiperSlide>
         ))}
       </Swiper>
