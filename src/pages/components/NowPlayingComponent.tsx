a602d77f3b13f8ea31120f284cff0ec123534c96
Louisse Evale <louissemevale@gmail.com>
2024-07-04 13:08:23 +0800

    feat(NowPlayingComponent): use ClickableDivComponent and onClickMovieItem

--- src/pages/components/NowPlayingComponent.tsx
+++ src/pages/components/NowPlayingComponent.tsx
@@ -1,7 +1,9 @@
+import { ClickableDivComponent } from "@GlobalComponents"
+import { MovieItem } from "@Store"
+import { useCommonMethods } from "@Utilities"
 import { map } from "lodash-es"
 import { Autoplay } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
-import { MovieItem } from "@Modules"
 import styles from "./scss/styles.module.scss"
 
 import "swiper/css"
@@ -14,39 +16,32 @@ type Props = {
 
 function NowPlayingComponent(props: Props) {
   const { data } = props
+  const { onClickMovieItem } = useCommonMethods()
 
   return (
     <div className={styles.nowPlayingComponent}>
       <p className={styles.title}>Now Playing in Cinemas</p>
       <Swiper
         breakpoints={{
-          700: {
-            slidesPerView: 2,
-            spaceBetween: 16
-          },
-          600: {
-            slidesPerView: 2,
-            spaceBetween: 8
-          },
-          0: {
-            slidesPerView: 1
-          }
+          700: { slidesPerView: 2, spaceBetween: 16 },
+          600: { slidesPerView: 2, spaceBetween: 8 },
+          0: { slidesPerView: 1 }
         }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false
-        }}
+        autoplay={{ delay: 3000 }}
         modules={[Autoplay]}
         className={styles.swiper}
       >
         {map(data, (item) => (
           <SwiperSlide key={item.id}>
-            <div className={styles.swiper__item}>
+            <ClickableDivComponent
+              className={styles.swiper__item}
+              onClick={() => onClickMovieItem(item.id)}
+            >
               <img src={item.backdrop_url} alt={`${item.title} Poster`} />
               <div className={styles.textWrapper}>
                 <p>{item.title}</p>
               </div>
-            </div>
+            </ClickableDivComponent>
           </SwiperSlide>
         ))}
       </Swiper>
