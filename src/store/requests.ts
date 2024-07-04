1ec7b1a33418161e1b71d31479b0854689afbbc5
Louisse Evale <louissemevale@gmail.com>
2024-07-04 12:50:46 +0800

    refactor(store/requests.ts): remove data transformation in each requests

--- src/store/requests.ts
+++ src/store/requests.ts
@@ -1,16 +1,11 @@
-import { map } from "lodash-es"
 import { API } from "@APIService"
-import { getPosterURL } from "@Utilities"
 import {
   GenreListResponse,
-  NowPlayingResponse,
-  PopularResponse,
-  TopRatedResponse,
-  UpcomingResponse
+  MovieDetailsResponse,
+  MoviesListResponse
 } from "./types"
 
 async function getGenreList() {
-  let data = null
   const endpoint = "genre/movie/list"
 
   try {
@@ -18,116 +13,88 @@ async function getGenreList() {
       method: "get",
       endpoint
     })
-    data = response.data
-  } catch (err) {
-    throw new Error(`Failed to get data from ${endpoint}.`)
-  }
-
-  return data.genres
-}
-
-async function getNowPlaying() {
-  let data = null
-  const endpoint = "movie/now_playing"
-
-  try {
-    const response = await API.request<NowPlayingResponse>({
-      method: "get",
-      endpoint
-    })
-    data = response.data
+    const { data } = response
+    return data.genres
   } catch (error) {
     throw new Error(`Failed to get data from ${endpoint}.`)
   }
+}
 
-  const { results } = data
-  const updatedResults = map(results, (item) => ({
-    ...item,
-    backdrop_url: getPosterURL(item.backdrop_path),
-    poster_url: getPosterURL(item.poster_path)
-  }))
+async function getNowPlaying() {
+  const endpoint = "movie/now_playing"
 
-  return updatedResults
+  try {
+    const response = await API.request<MoviesListResponse>({
+      method: "get",
+      endpoint
+    })
+    return response.data
+  } catch (error) {
+    throw new Error(`Failed to get data from ${endpoint}.`)
+  }
 }
 
 async function getPopular() {
-  let data = null
   const endpoint = "movie/popular"
 
   try {
-    const response = await API.request<PopularResponse>({
+    const response = await API.request<MoviesListResponse>({
       method: "get",
       endpoint,
       params: {
         page: 2
       }
     })
-    data = response.data
+    return response.data
   } catch (error) {
     throw new Error(`Failed to get data from ${endpoint}.`)
   }
-
-  const { results } = data
-  const slicedResults = results.slice(0, 6)
-  const updatedResults = map(slicedResults, (item) => ({
-    ...item,
-    backdrop_url: getPosterURL(item.backdrop_path),
-    poster_url: getPosterURL(item.poster_path)
-  }))
-
-  return updatedResults
-}
-
-async function getUpcoming() {
-  let data = null
-  const endpoint = "movie/upcoming"
-
-  try {
-    const response = await API.request<UpcomingResponse>({
-      method: "get",
-      endpoint
-    })
-    data = response.data
-  } catch (error) {
-    throw new Error(`Failed to get data from ${endpoint}.`)
-  }
-
-  const { results } = data
-  const slicedResults = results.slice(0, 4)
-  const updatedResults = map(slicedResults, (item) => ({
-    ...item,
-    backdrop_url: getPosterURL(item.backdrop_path),
-    poster_url: getPosterURL(item.poster_path)
-  }))
-
-  return updatedResults
 }
 
 async function getTopRated() {
-  let data = null
   const endpoint = "movie/top_rated"
 
   try {
-    const response = await API.request<TopRatedResponse>({
+    const response = await API.request<MoviesListResponse>({
       method: "get",
       endpoint
     })
-    data = response.data
+    return response.data
   } catch (error) {
     throw new Error(`Failed to get data from ${endpoint}.`)
   }
+}
 
-  const { results } = data
-  const updatedResults = map(results, (item) => ({
-    ...item,
-    backdrop_url: getPosterURL(item.backdrop_path),
-    poster_url: getPosterURL(item.poster_path)
-  }))
+async function getUpcoming() {
+  const endpoint = "movie/upcoming"
 
-  return updatedResults
+  try {
+    const response = await API.request<MoviesListResponse>({
+      method: "get",
+      endpoint
+    })
+    return response.data
+  } catch (error) {
+    throw new Error(`Failed to get data from ${endpoint}.`)
+  }
+}
+
+async function getDetails(id: string | undefined) {
+  const endpoint = `movie/${id}`
+
+  try {
+    const response = await API.request<MovieDetailsResponse>({
+      method: "get",
+      endpoint
+    })
+    return response.data
+  } catch (error) {
+    throw new Error(`Failed to get data from ${endpoint}.`)
+  }
 }
 
 export {
+  getDetails,
   getGenreList,
   getNowPlaying,
   getPopular,
