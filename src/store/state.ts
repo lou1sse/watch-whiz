09e9a3d5e20bbd7da93eecc016ce71df60ca4d8b
Louisse Evale <louissemevale@gmail.com>
2024-07-04 12:50:04 +0800

    feat(store/state.ts): add details and setDetails, specify types for initial states

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,19 +1,26 @@
 import { create } from "zustand"
-import { MoviesState } from "./types"
+import {
+  GenreItem,
+  MovieDetailsResponse,
+  MovieItem,
+  MoviesState
+} from "./types"
 
-const useStore = create<MoviesState>((set) => ({
-  genreList: [],
-  nowPlaying: [],
-  popular: [],
-  upcoming: [],
-  topRated: [],
+const useMovieStore = create<MoviesState>((set) => ({
+  genreList: [] as GenreItem[],
+  nowPlaying: [] as MovieItem[],
+  popular: [] as MovieItem[],
+  upcoming: [] as MovieItem[],
+  topRated: [] as MovieItem[],
+  details: {} as MovieDetailsResponse,
 
   /* actions */
   setGenreList: (genreList) => set({ genreList }),
   setNowPlaying: (nowPlaying) => set({ nowPlaying }),
   setPopular: (popular) => set({ popular }),
   setUpcoming: (upcoming) => set({ upcoming }),
-  setTopRated: (topRated) => set({ topRated })
+  setTopRated: (topRated) => set({ topRated }),
+  setDetails: (details) => set({ details })
 }))
 
-export default useStore
+export default useMovieStore
