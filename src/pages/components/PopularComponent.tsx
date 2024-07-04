19a55214b001a6c49fee118de4d01e95ea2edeed
Louisse Evale <louissemevale@gmail.com>
2024-07-04 13:09:40 +0800

    feat(PopularComponent): use ClickableDivComponent and onClickMovieItem

--- src/pages/components/PopularComponent.tsx
+++ src/pages/components/PopularComponent.tsx
@@ -1,5 +1,7 @@
+import { ClickableDivComponent } from "@GlobalComponents"
+import { MovieItem } from "@Store"
+import { useCommonMethods } from "@Utilities"
 import { map } from "lodash-es"
-import { MovieItem } from "@Modules"
 import styles from "./scss/styles.module.scss"
 
 type Props = {
@@ -8,16 +10,21 @@ type Props = {
 
 function PopularComponent(props: Props) {
   const { data } = props
+  const { onClickMovieItem } = useCommonMethods()
 
   return (
     <div className={styles.popularComponent}>
       <p className={styles.title}>Popular Now</p>
       <div className={styles.movies}>
-        {map(data, (item, index) => (
-          <div key={index} className={styles.movies__item}>
+        {map(data, (item) => (
+          <ClickableDivComponent
+            key={item.id}
+            className={styles.movies__item}
+            onClick={() => onClickMovieItem(item.id)}
+          >
             <img src={item.backdrop_url} alt={`${item.title} Poster`} />
             <p>{item.title}</p>
-          </div>
+          </ClickableDivComponent>
         ))}
       </div>
     </div>
