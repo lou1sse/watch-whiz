40ec9a548cb6d70a2fa02b0db29d3cae4a67a61c
Louisse Evale <louissemevale@gmail.com>
2024-07-04 13:09:11 +0800

    feat(UpcomingComponent): use ClickableDivComponent and onClickMovieItem

--- src/pages/components/UpcomingComponent.tsx
+++ src/pages/components/UpcomingComponent.tsx
@@ -1,6 +1,7 @@
+import { ClickableDivComponent } from "@GlobalComponents"
+import { MovieItem } from "@Store"
+import { formatDate, useCommonMethods } from "@Utilities"
 import { map } from "lodash-es"
-import { MovieItem } from "@Modules"
-import { formatDate } from "@Utilities"
 import styles from "./scss/styles.module.scss"
 
 type Props = {
@@ -9,13 +10,18 @@ type Props = {
 
 function UpcomingComponent(props: Props) {
   const { data } = props
+  const { onClickMovieItem } = useCommonMethods()
 
   return (
     <div className={styles.upcomingComponent}>
       <p className={styles.title}>Upcoming Movies</p>
       <div className={styles.movies}>
-        {map(data, (item, index) => (
-          <div key={index} className={styles.movies__item}>
+        {map(data, (item) => (
+          <ClickableDivComponent
+            key={item.id}
+            className={styles.movies__item}
+            onClick={() => onClickMovieItem(item.id)}
+          >
             <img src={item.backdrop_url} alt={`${item.title} Poster`} />
             <div className={styles.textWrapper}>
               <p>{item.title}</p>
@@ -23,7 +29,7 @@ function UpcomingComponent(props: Props) {
                 Release date: {formatDate(item.release_date)}
               </p>
             </div>
-          </div>
+          </ClickableDivComponent>
         ))}
       </div>
     </div>
