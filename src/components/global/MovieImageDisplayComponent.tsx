ede3688c9c54c00f381b694bbf98db2a43f97261
Louisse Evale <louissemevale@gmail.com>
2024-07-12 20:47:33 +0800

    feat(components/global): create MovieImageDisplayComponent

---
+++ src/components/global/MovieImageDisplayComponent.tsx
@@ -0,0 +1,58 @@
+import classNames from "classnames"
+import styles from "./scss/styles.module.scss"
+
+type Props = {
+  imageSource: string,
+  title: string,
+  releaseDate: string,
+  altSuffix?: string,
+  width?: string,
+  height?: string,
+  showOverlay?: boolean,
+  showPlaceholder?: boolean
+}
+
+function MovieImageDisplayComponent(props: Props) {
+  const {
+    imageSource,
+    altSuffix = "Poster",
+    title,
+    releaseDate,
+    width,
+    height = "270px",
+    showOverlay,
+    showPlaceholder
+  } = props
+
+  const renderPlaceholder = () => (
+    <div
+      className={classNames(styles.placeholder, {
+        [styles.overlay]: showOverlay
+      })}
+    >
+      <p className={styles.title}>{title}</p>
+      <p>{releaseDate}</p>
+    </div>
+  )
+
+  return (
+    <div
+      className={styles.movieImageDisplayComponent}
+      style={{
+        width,
+        height,
+        minWidth: width,
+        maxWidth: width,
+        minHeight: height,
+        maxHeight: height
+      }}
+    >
+      {(showPlaceholder || showOverlay) && renderPlaceholder()}
+      {!showPlaceholder && (
+        <img src={imageSource} alt={`${title} ${altSuffix}`} />
+      )}
+    </div>
+  )
+}
+
+export default MovieImageDisplayComponent
--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,4 +1,5 @@
 export { default as ClickableDivComponent } from "./ClickableDivComponent"
+export { default as MovieImageDisplayComponent } from "./MovieImageDisplayComponent"
 export { default as SearchComponent } from "./SearchComponent"
 export { default as TabComponent } from "./TabComponent"
 export { default as TooltipComponent } from "./TooltipComponent"
