fcdd5b5b7b6ff8e65ed031fccde14fe5d441abb2
Louisse Evale <louissemevale@gmail.com>
2024-07-19 21:15:16 +0800

    feat(DropdownItemComponent): add new optional props - className and onClick, and remove union type with HTMLAttributes<HTMLDivElement>

--- src/components/compound/DropdownComponent/components/DropdownItemComponent.tsx
+++ src/components/compound/DropdownComponent/components/DropdownItemComponent.tsx
@@ -2,16 +2,16 @@ import { ClickableDivComponent } from "@GlobalComponents"
 import { MenuItem } from "@headlessui/react"
 import classNames from "classnames"
 import { isObject } from "lodash-es"
-import { HTMLAttributes, PropsWithChildren, useMemo } from "react"
+import { PropsWithChildren, useMemo } from "react"
 import { DropdownItem, useDropdownContext } from "../DropdownContext"
 import styles from "./scss/styles.module.scss"
 
-type Props<T extends DropdownItem> = PropsWithChildren<
-  {
-    data?: T,
-    disabled?: boolean
-  } & HTMLAttributes<HTMLDivElement>
->
+type Props<T extends DropdownItem> = PropsWithChildren<{
+  data?: T,
+  disabled?: boolean,
+  className?: string,
+  onClick?: () => void
+}>
 
 function DropdownItemComponent<T extends DropdownItem>(props: Props<T>) {
   const { itemLabelKey, setSelectedItem } = useDropdownContext()
@@ -27,6 +27,11 @@ function DropdownItemComponent<T extends DropdownItem>(props: Props<T>) {
 
   const onClickItem = () => {
     if (disabled) return
+
+    if (onClick) {
+      onClick()
+    }
+
     setSelectedItem(data)
   }
 
