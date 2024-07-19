d91387e8d8411a13b0eceee34f34cbcde81c97ec
Louisse Evale <louissemevale@gmail.com>
2024-07-19 21:23:02 +0800

    feat(constants): create FILTERS, change route values of HEADER_NAV_LINKS and FOOTER_NAV_LINKS

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -13,26 +13,26 @@ const HEADER_NAV_LINKS = [
   },
   {
     label: "Films",
-    route: "/films"
-  },
-  {
-    label: "Upcoming",
-    route: "/upcoming"
+    route: "/search"
   },
   {
     label: "Popular",
-    route: "/popular"
+    route: "/search"
+  },
+  {
+    label: "Upcoming",
+    route: "/search"
   }
 ]
 
 const FOOTER_NAV_LINKS = [
   {
-    label: "Upcoming",
-    route: "/upcoming"
+    label: "Popular",
+    route: "/search"
   },
   {
-    label: "Popular",
-    route: "/popular"
+    label: "Upcoming",
+    route: "/search"
   },
   {
     label: "About",
@@ -70,6 +70,66 @@ const MORE_DETAILS_TABS = [
   }
 ]
 
+/* FILTERS */
+const SORT_BY = [
+  { id: "film_name", payload: "title.asc", label: "Film Name" },
+  { label: "Release date" },
+  {
+    id: "earliest_release",
+    payload: "release_date.asc",
+    label: "Earliest first",
+    section: "Release date"
+  },
+  {
+    id: "newest_release",
+    payload: "release_date.desc",
+    label: "Newest first",
+    section: "Release date"
+  },
+  { label: "Rating" },
+  {
+    id: "lowest_rating",
+    payload: "vote_average.asc",
+    label: "Lowest first",
+    section: "Rating"
+  },
+  {
+    id: "highest_rating",
+    payload: "vote_average.desc",
+    label: "Highest first",
+    section: "Rating"
+  },
+  { label: "Popularity" },
+  {
+    id: "all_time",
+    payload: "popularity.desc",
+    label: "All Time",
+    section: "Popularity"
+  },
+  {
+    id: "year",
+    payload: "popularity.desc",
+    label: "This year",
+    section: "Popularity"
+  },
+  {
+    id: "month",
+    payload: "popularity.desc",
+    label: "This month",
+    section: "Popularity"
+  },
+  {
+    id: "week",
+    payload: "popularity.desc",
+    label: "This week",
+    section: "Popularity"
+  }
+]
+
+const FILTERS = {
+  SORT_BY
+}
+
 export {
   API_URL,
   API_KEY,
@@ -78,5 +138,6 @@ export {
   HEADER_NAV_LINKS,
   FOOTER_NAV_LINKS,
   INFO_LINKS,
-  MORE_DETAILS_TABS
+  MORE_DETAILS_TABS,
+  FILTERS
 }
