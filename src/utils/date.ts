a345fdacc0579716b485791474820829ee51e167
Louisse Evale <louissemevale@gmail.com>
2024-07-22 19:50:07 +0800

    feat(utils/date.ts): create getDateRange

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,4 +4,14 @@ function formatDate(date: string | Moment | Date, pattern?: string) {
   return moment(date).format(pattern || "MMMM DD, YYYY")
 }
 
-export { formatDate }
+function getDateRange(period: "week" | "month", pattern = "YYYY-MM-DD") {
+  const startDate = moment().startOf(period).format(pattern)
+  const endDate = moment().endOf(period).format(pattern)
+
+  return {
+    startDate,
+    endDate
+  }
+}
+
+export { formatDate, getDateRange }
